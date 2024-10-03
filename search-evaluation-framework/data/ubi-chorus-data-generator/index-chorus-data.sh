197fdaa87921e140feade7fbf6f6bc28f5de5322
jzonthemtn <jeff.zemerick@mtnfog.com>
2024-10-03 10:10:14 -0400

    Increasing fields.

--- search-evaluation-framework/data/ubi-chorus-data-generator/index-chorus-data.sh
+++ search-evaluation-framework/data/ubi-chorus-data-generator/index-chorus-data.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
 
 curl -s -X PUT "http://localhost:9200/ecommerce/" -H 'Content-Type: application/json' --data-binary @./schema.json
-curl -s -X PUT "http://localhost:9200/ecommerce/_settings"  -H 'Content-Type: application/json' -d '{"index.mapping.total_fields.limit": 2000}'
+curl -s -X PUT "http://localhost:9200/ecommerce/_settings"  -H 'Content-Type: application/json' -d '{"index.mapping.total_fields.limit": 5000}'
 curl -s -X POST "http://localhost:9200/ecommerce/_bulk?pretty=false&filter_path=-items" -H 'Content-Type: application/json' --data-binary @transformed_data.json
