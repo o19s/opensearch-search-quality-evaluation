29530b7192ed5db151a6c9d52644e1c1d3e26a39
Alexey Rodriguez Yakushev <alexey.rodriguez@gmail.com>
2024-10-09 12:19:37 +0200

    Put data generation and vega dashboard generation in their own directories

--- search-evaluation-framework/opensearch-dashboard-prototyping/vega_dashboards/4_generate_vega_dashboards.py
+++ search-evaluation-framework/opensearch-dashboard-prototyping/vega_dashboards/4_generate_vega_dashboards.py
@@ -7,10 +7,10 @@ plots = ["scatter_plot", "violin_plot"]
 
 parser = argparse.ArgumentParser(description='Vega Dashboard generator')
 parser.add_argument("--test", action="store_true")
-parser.add_argument("--plot", required=True, help="plot to generate, one of: f{plots}")
+parser.add_argument("--plot", required=True, help=f"plot to generate, one of: {plots}")
 args = parser.parse_args()
 
-dfm = pd.read_csv("metrics.csv")
+dfm = pd.read_csv("../data_notebooks/metrics.csv")
 
 def metrics_to_vega_data(df):
     rows = []
@@ -27,14 +27,14 @@ def metrics_to_vega_data(df):
 
 assert args.plot in plots
 
-with open(f"vega_templates/{args.plot}_vega.json") as f:
+with open(f"templates/{args.plot}_vega.json") as f:
     dash = json.load(f)
 
 if args.test:
     jmetrics = metrics_to_vega_data(dfm.head(10))
     dash["data"] = jmetrics
 else:
-    with open(f"vega_templates/{args.plot}_data.json") as f:
+    with open(f"templates/{args.plot}_data.json") as f:
         jmetrics = json.load(f)
         dash["data"] = jmetrics["data"]
         dash["transform"] = jmetrics["transform"] + dash["transform"]
