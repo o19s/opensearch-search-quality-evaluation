640b0adc36f14dcadf4c9e7afd3e7506cf58a91d
jzonthemtn <jeff.zemerick@mtnfog.com>
2025-01-02 10:58:45 -0500

    Updating paths.

--- cli-runner/run.sh
+++ cli-runner/run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -e
 
 # Create a click model.
-java -jar ./target/search-evaluation-framework-1.0.0-SNAPSHOT-jar-with-dependencies.jar -c coec
+java -jar ../target/search-evaluation-framework-1.0.0-SNAPSHOT-jar-with-dependencies.jar -c coec
 
 # Create a query set using sampling.
-#java -jar ./target/search-evaluation-framework-1.0.0-SNAPSHOT-jar-with-dependencies.jar -s sampling.json
+#java -jar ../target/search-evaluation-framework-1.0.0-SNAPSHOT-jar-with-dependencies.jar -s sampling.json
 
 # Run a query set.
-#java -jar ./target/search-evaluation-framework-1.0.0-SNAPSHOT-jar-with-dependencies.jar -r queryset.json
+#java -jar ../target/search-evaluation-framework-1.0.0-SNAPSHOT-jar-with-dependencies.jar -r queryset.json
