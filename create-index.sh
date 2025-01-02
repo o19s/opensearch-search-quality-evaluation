b2ca178fb14a9ef8db5d58181902891faddece1e
jzonthemtn <jeff.zemerick@mtnfog.com>
2025-01-02 10:51:44 -0500

    Moving scripts.
    Signed-off-by: jzonthemtn <jeff.zemerick@mtnfog.com>

--- create-index.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash -e
-
-echo "Creating ubi_events index"
-
-curl -X DELETE http://localhost:9200/ubi_events
-curl -X PUT http://localhost:9200/ubi_events -H "Content-Type: application/json"
-curl -X PUT http://localhost:9200/ubi_events/_mapping -H "Content-Type: application/json" -d @./events-mapping.json
