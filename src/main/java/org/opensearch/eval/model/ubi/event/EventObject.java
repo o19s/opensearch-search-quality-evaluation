d4a3d1747673ce8df615aa497bb1a2ea6dfda70a
jzonthemtn <jeff.zemerick@mtnfog.com>
2025-01-22 09:29:08 -0500

    Adding additionalProperties.

--- src/main/java/org/opensearch/eval/model/ubi/event/EventObject.java
+++ src/main/java/org/opensearch/eval/model/ubi/event/EventObject.java
@@ -8,8 +8,12 @@
  */
 package org.opensearch.eval.model.ubi.event;
 
+import com.fasterxml.jackson.annotation.JsonAnySetter;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.google.gson.annotations.SerializedName;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class EventObject {
 
@@ -20,6 +24,8 @@ public class EventObject {
     @JsonProperty("object_id")
     @SerializedName("object_id")
     private String objectId;
+
+    private final Map<String, Object> additionalProperties = new HashMap<>();
 
     @Override
     public String toString() {
@@ -56,6 +62,24 @@ public class EventObject {
      */
     public void setObjectIdField(String objectIdField) {
         this.objectIdField = objectIdField;
+    }
+
+    /**
+     * Adds an unrecognized property to the additional properties map.
+     * @param key   The property name.
+     * @param value The property value.
+     */
+    @JsonAnySetter
+    public void addAdditionalProperty(String key, Object value) {
+        this.additionalProperties.put(key, value);
+    }
+
+    /**
+     * Gets the additional properties.
+     * @return The additional properties map.
+     */
+    public Map<String, Object> getAdditionalProperties() {
+        return additionalProperties;
     }
 
 }
