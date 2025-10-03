4747fa33504c422863c4430aab8fd94130bb480d
Eric Pugh <epugh@opensourceconnections.com>
2025-10-03 07:31:30 -0400

    Use SCRIPT_DIR for file paths in install_dashboards.sh
    allow you to run the script from any directory.

--- opensearch-dashboard-prototyping/install_dashboards.sh
+++ opensearch-dashboard-prototyping/install_dashboards.sh
@@ -5,6 +5,9 @@ MAJOR='\033[0;34m'
 MINOR='\033[0;37m    '
 RESET='\033[0m' # No Color
 
+# Get the directory where this script is located
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
 opensearch=$1
 opensearch_dashboard=$2
 set -eo pipefail
@@ -24,13 +27,13 @@ echo "${MAJOR}Deleting index srw_metrics_sample_data${RESET}"
 curl -s -X DELETE $opensearch/srw_metrics_sample_data/
 
 echo "${MAJOR}Creating index srw_metrics_sample_data with mappings${RESET}"
-curl -s -X PUT $opensearch/srw_metrics_sample_data/ -H "Content-Type: application/json" -d @srw_metrics_mappings.json
+curl -s -X PUT $opensearch/srw_metrics_sample_data/ -H "Content-Type: application/json" -d @"$SCRIPT_DIR/srw_metrics_mappings.json"
 
      
 echo "${MAJOR}Populating index srw_metrics_sample_data with sample metric data${RESET}"
-curl -s -H 'Content-Type: application/x-ndjson' -XPOST "$opensearch/srw_metrics_sample_data/_bulk?pretty=false&filter_path=-items" --data-binary @sample_data.ndjson 
+curl -s -H 'Content-Type: application/x-ndjson' -XPOST "$opensearch/srw_metrics_sample_data/_bulk?pretty=false&filter_path=-items" --data-binary @"$SCRIPT_DIR/sample_data.ndjson" 
 
 echo "${MAJOR}\nInstalling Quality Evaluation Framework Dashboards${RESET}"
-curl -X POST "$opensearch_dashboard/api/saved_objects/_import?overwrite=true" -H "osd-xsrf: true" --form file=@search_dashboard.ndjson > /dev/null
+curl -X POST "$opensearch_dashboard/api/saved_objects/_import?overwrite=true" -H "osd-xsrf: true" --form file=@"$SCRIPT_DIR/search_dashboard.ndjson" > /dev/null
 
 echo "${MAJOR}The Quality Evaluation Framework Dashboards were successfully installed${RESET}"
